b9538241a1919e7e46219102369d0023dff75961
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-02-07 19:00:01 +0600

    #10 Refactor code and add unit tests

---
+++ awssqs/main_test.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/service/sqs/sqsiface"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockedReceiveMsgs struct {
+	sqsiface.SQSAPI
+	Resp sqs.ReceiveMessageOutput
+}
+
+type mockedDeleteMsgs struct {
+	sqsiface.SQSAPI
+	Resp sqs.DeleteMessageOutput
+}
+
+type mockedListQueues struct {
+	sqsiface.SQSAPI
+	Resp sqs.ListQueuesOutput
+}
+
+func (m mockedReceiveMsgs) ReceiveMessage(in *sqs.ReceiveMessageInput) (*sqs.ReceiveMessageOutput, error) {
+	return &m.Resp, nil
+}
+
+func (m mockedDeleteMsgs) DeleteMessage(in *sqs.DeleteMessageInput) (*sqs.DeleteMessageOutput, error) {
+	return &m.Resp, nil
+}
+
+func (m mockedListQueues) ListQueues(in *sqs.ListQueuesInput) (*sqs.ListQueuesOutput, error) {
+	return &m.Resp, nil
+}
+
+func TestQueueLookup(t *testing.T) {
+	cases := []struct {
+		Resp     sqs.ListQueuesOutput
+		Expected string
+	}{
+		{ // Case 1, expect parsed responses
+			Resp: sqs.ListQueuesOutput{
+				QueueUrls: []*string{aws.String("testQueueURL")}},
+			Expected: "testQueueURL",
+		},
+		{ // Case 1, expect parsed responses
+			Resp: sqs.ListQueuesOutput{
+				QueueUrls: []*string{aws.String("testQueueURL"), aws.String("testQueueURLFoo")}},
+			Expected: "testQueueURL",
+		},
+	}
+
+	for _, c := range cases {
+		q := Queue{
+			Client: mockedListQueues{Resp: c.Resp},
+		}
+		url, err := q.QueueLookup("testQueue")
+		assert.NoError(t, err)
+		assert.Equal(t, c.Expected, url)
+	}
+}
+
+func TestGetMessages(t *testing.T) {
+	cases := []struct {
+		Resp     sqs.ReceiveMessageOutput
+		Expected []sqs.Message
+	}{
+		{ // Case 1, expect parsed responses
+			Resp: sqs.ReceiveMessageOutput{
+				Messages: []*sqs.Message{
+					{},
+				},
+			},
+			Expected: []sqs.Message{
+				{},
+			},
+		},
+		{ // Case 2, not messages returned
+			Resp:     sqs.ReceiveMessageOutput{},
+			Expected: []sqs.Message{},
+		},
+	}
+
+	for _, c := range cases {
+		q := Queue{
+			Client: mockedReceiveMsgs{Resp: c.Resp},
+			URL:    aws.String("mockURL"),
+		}
+		msgs, err := q.GetMessages(20)
+		assert.NoError(t, err)
+		assert.Equal(t, len(c.Expected), len(msgs))
+	}
+}
+
+func TestPushMessage(t *testing.T) {
+	msg := sqs.Message{}
+	err := pushMessage(&msg)
+	assert.Error(t, err)
+
+	msg = sqs.Message{
+		MessageId: aws.String("foo"),
+		Body:      aws.String("bar"),
+		Attributes: map[string]*string{
+			"SentTimestamp": aws.String("1549540781"),
+		},
+	}
+	dryRun = true
+	err = pushMessage(&msg)
+	assert.NoError(t, err)
+}
+
+func TestDeleteMessage(t *testing.T) {
+
+	q := Queue{
+		Client: mockedDeleteMsgs{Resp: sqs.DeleteMessageOutput{}},
+		URL:    aws.String("mockURL"),
+	}
+
+	msg := sqs.Message{
+		ReceiptHandle: aws.String("foo"),
+	}
+	err := q.DeleteMessage(msg.ReceiptHandle)
+	assert.NoError(t, err)
+}
