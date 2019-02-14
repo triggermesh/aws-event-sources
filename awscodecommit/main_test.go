d9018e75dcc1373a541dd021dddffd60b61d2082
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-02-14 11:52:38 +0600

    Add tests for send cloudevent func, fix code return statement

--- awscodecommit/main_test.go
+++ awscodecommit/main_test.go
@@ -5,6 +5,9 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/codecommit"
 	"github.com/aws/aws-sdk-go/service/codecommit/codecommitiface"
+	"github.com/jarcoal/httpmock"
+	"github.com/knative/pkg/cloudevents"
+	"github.com/stretchr/testify/assert"
 )
 
 type mockedClientForCommits struct {
@@ -40,11 +43,74 @@ func (m mockedClientForPRs) GetPullRequests(in *codecommit.GetPullRequestInput) 
 }
 
 func TestSendPREvent(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
 
+	httpmock.RegisterResponder("POST", "https://foo.com", httpmock.NewStringResponder(200, ``))
+	httpmock.RegisterResponder("POST", "https://bar.com", httpmock.NewStringResponder(500, ``))
+
+	client := СodeCommitClient{
+		CloudEventsClient: cloudevents.NewClient(
+			"https://bar.com",
+			cloudevents.Builder{
+				Source:    "aws:codecommit",
+				EventType: "codecommit event",
+			},
+		),
+	}
+
+	pullRequest := codecommit.PullRequest{}
+
+	err := client.sendPREvent(&pullRequest)
+	assert.Error(t, err)
+
+	c := cloudevents.NewClient(
+		"https://foo.com",
+		cloudevents.Builder{
+			Source:    "aws:codecommit",
+			EventType: "codecommit event",
+		},
+	)
+
+	client.CloudEventsClient = c
+
+	err = client.sendPREvent(&pullRequest)
+	assert.NoError(t, err)
 }
 
 func TestSendCommitEvent(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
 
+	httpmock.RegisterResponder("POST", "https://foo.com", httpmock.NewStringResponder(200, ``))
+	httpmock.RegisterResponder("POST", "https://bar.com", httpmock.NewStringResponder(500, ``))
+
+	client := СodeCommitClient{
+		CloudEventsClient: cloudevents.NewClient(
+			"https://bar.com",
+			cloudevents.Builder{
+				Source:    "aws:codecommit",
+				EventType: "codecommit event",
+			},
+		),
+	}
+
+	commit := codecommit.Commit{}
+	err := client.sendCommitEvent(&commit)
+	assert.Error(t, err)
+
+	c := cloudevents.NewClient(
+		"https://foo.com",
+		cloudevents.Builder{
+			Source:    "aws:codecommit",
+			EventType: "codecommit event",
+		},
+	)
+
+	client.CloudEventsClient = c
+
+	err = client.sendCommitEvent(&commit)
+	assert.NoError(t, err)
 }
 
 func TestProcessCommits(t *testing.T) {
