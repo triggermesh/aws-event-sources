bbaf4412eb90ab764eba71fe3b0458512b611d9e
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-02-15 14:44:16 +0600

    Refactor source code, remove 3rd party code, improve readability
    Finish refactoring code for AWS Dynamo DB. removed external 3rd
    party library out of the code, now it is plain Go SDK usage. Now
    need to organize everything in functions and unit test it after that

--- awsdynamodb/main.go
+++ awsdynamodb/main.go
@@ -24,79 +24,176 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodbstreams"
-	"github.com/triggermesh/sources/tmevents"
-	"github.com/urakozz/go-dynamodb-stream-subscriber/stream"
+	"github.com/aws/aws-sdk-go/service/dynamodbstreams/dynamodbstreamsiface"
+	"github.com/knative/pkg/cloudevents"
 
 	log "github.com/sirupsen/logrus"
 )
 
-var sink string
+//DynamoDBEvent represents AWS Dynamo DB payload
+type DynamoDBEvent struct {
+	AwsRegion    *string                       `locationName:"awsRegion" type:"string"`
+	Dynamodb     *dynamodbstreams.StreamRecord `locationName:"dynamodb" type:"structure"`
+	EventID      *string                       `locationName:"eventID" type:"string"`
+	EventName    *string                       `locationName:"eventName" type:"string" enum:"OperationType"`
+	EventSource  *string                       `locationName:"eventSource" type:"string"`
+	EventVersion *string                       `locationName:"eventVersion" type:"string"`
+	UserIdentity *dynamodbstreams.Identity     `locationName:"userIdentity" type:"structure"`
+}
+
+//Client struct represent all clients
+type Client struct {
+	StreamsClient dynamodbstreamsiface.DynamoDBStreamsAPI
+	CloudEvents   *cloudevents.Client
+}
+
+var (
+	sink                   string
+	accountAccessKeyID     string
+	accountSecretAccessKey string
+	accountRegion          string
+	tableName              string
+)
 
 func init() {
 	flag.StringVar(&sink, "sink", "", "where to sink events to")
+	accountAccessKeyID = os.Getenv("AWS_ACCESS_KEY_ID")
+	accountSecretAccessKey = os.Getenv("AWS_SECRET_ACCESS_KEY")
+	accountRegion = os.Getenv("AWS_REGION")
+	tableName = os.Getenv("TABLE")
 }
 
 func main() {
 
 	flag.Parse()
 
-	accountAccessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
-	accountSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
-	accountSecureToken := os.Getenv("AWS_SECURE_TOKEN")
-	accountRegion := os.Getenv("AWS_REGION")
-	tableName := os.Getenv("TABLE")
-
-	cfg := aws.NewConfig().WithRegion(accountRegion)
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String(accountRegion),
-		Credentials: credentials.NewStaticCredentials(accountAccessKeyID, accountSecretAccessKey, accountSecureToken),
+		Credentials: credentials.NewStaticCredentials(accountAccessKeyID, accountSecretAccessKey, ""),
 	})
+
 	if err != nil {
-		log.Errorf("NewSession failed: %v", err)
-		return
+		log.Fatalf("NewSession failed: %v", err)
 	}
-	streamSvc := dynamodbstreams.New(sess, cfg)
-	dynamoSvc := dynamodb.New(sess, cfg)
-	table := tableName
 
-	log.Info("Begin listening for Dynamo DB Stream")
+	streamsClient := dynamodbstreams.New(sess)
 
-	streamSubscriber := stream.NewStreamSubscriber(dynamoSvc, streamSvc, table)
-	ch, errCh := streamSubscriber.GetStreamDataAsync()
+	cloudEvents := cloudevents.NewClient(
+		sink,
+		cloudevents.Builder{
+			Source:    "aws:dynamodb",
+			EventType: "dynamodb event",
+		},
+	)
 
-	go func(errCh <-chan error) {
-		for err := range errCh {
-			log.Errorf("Stream Subscriber error: %v", err)
-		}
-	}(errCh)
+	client := Client{
+		StreamsClient: streamsClient,
+		CloudEvents:   cloudEvents,
+	}
 
-	for record := range ch {
-		err := sendCloudevent(record, sink)
+	streams := []*dynamodbstreams.Stream{}
+
+	listStreamsInput := dynamodbstreams.ListStreamsInput{
+		TableName: aws.String(tableName),
+	}
+
+	for {
+		listStreamOutput, err := client.StreamsClient.ListStreams(&listStreamsInput)
 		if err != nil {
-			log.Errorf("SendCloudEvent failed: %v", err)
+			log.Error(err)
+		}
+
+		streams = append(streams, listStreamOutput.Streams...)
+
+		if listStreamOutput.LastEvaluatedStreamArn == nil {
+			break
+		}
+		listStreamsInput.ExclusiveStartStreamArn = listStreamOutput.LastEvaluatedStreamArn
+	}
+
+	streamDescriptions := []*dynamodbstreams.StreamDescription{}
+
+	for _, stream := range streams {
+
+		describeStreamOutput, err := client.StreamsClient.DescribeStream(&dynamodbstreams.DescribeStreamInput{
+			StreamArn: stream.StreamArn,
+		})
+
+		if err != nil {
+			log.Error(err)
+		}
+
+		streamDescriptions = append(streamDescriptions, describeStreamOutput.StreamDescription)
+	}
+
+	shardIterators := []*string{}
+
+	for _, streamDescription := range streamDescriptions {
+		for _, shard := range streamDescription.Shards {
+			getShardIteratorInput := dynamodbstreams.GetShardIteratorInput{
+				ShardId:           shard.ShardId,
+				ShardIteratorType: aws.String("LATEST"),
+				StreamArn:         streamDescription.StreamArn,
+			}
+
+			_, result := client.StreamsClient.GetShardIteratorRequest(&getShardIteratorInput)
+
+			shardIterators = append(shardIterators, result.ShardIterator)
 		}
 	}
+
+	// Get Records out of shard iterators.
+	records := []*dynamodbstreams.Record{}
+
+	for _, shardIterator := range shardIterators {
+		getRecordsInput := dynamodbstreams.GetRecordsInput{
+			ShardIterator: shardIterator,
+		}
+
+		for {
+
+			getRecordsOutput, err := client.StreamsClient.GetRecords(&getRecordsInput)
+			if err != nil {
+				log.Error(err)
+			}
+
+			records = append(records, getRecordsOutput.Records...)
+
+			if getRecordsOutput.NextShardIterator == nil {
+				break
+			}
+			getRecordsInput.ShardIterator = getRecordsOutput.NextShardIterator
+
+		}
+	}
+
+	for _, record := range records {
+		err := client.sendCloudevent(record)
+		if err != nil {
+			log.Error(err)
+		}
+	}
+
 }
 
-func sendCloudevent(record *dynamodbstreams.Record, sink string) error {
+func (c Client) sendCloudevent(record *dynamodbstreams.Record) error {
 
 	log.Info("Processing record ID: ", record.EventID)
 
-	eventInfo := tmevents.EventInfo{
-		EventData:   []byte(record.Dynamodb.String()),
-		EventID:     *record.EventID,
-		EventTime:   *record.Dynamodb.ApproximateCreationDateTime,
-		EventType:   "cloudevent.greet.you",
-		EventSource: *record.EventSource,
+	dynamoDBEvent := DynamoDBEvent{
+		AwsRegion:    record.AwsRegion,
+		Dynamodb:     record.Dynamodb,
+		EventID:      record.EventID,
+		EventName:    record.EventName,
+		EventSource:  record.EventSource,
+		EventVersion: record.EventVersion,
+		UserIdentity: record.UserIdentity,
 	}
 
-	log.Infof("Pushing record [%v] to [%v] ", record.EventID, sink)
-
-	err := tmevents.PushEvent(&eventInfo, sink)
-	if err != nil {
+	if err := c.CloudEvents.Send(dynamoDBEvent); err != nil {
 		return err
 	}
+
 	return nil
 }
