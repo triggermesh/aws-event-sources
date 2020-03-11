088ad0aeaaf63736d1fdea3addc416b693483f05
Alexander Edakin <alexeagro98@gmail.com>
2020-03-11 12:29:51 +0600

    update packages of awscognito (#28)
    Update Gopkg.lock
    Remove github.com/triggermesh/sources as unused dependency from Gopkg.toml
    Fix failing tests after update

--- awscognito/main.go
+++ awscognito/main.go
@@ -245,7 +245,7 @@ func (clients Clients) sendCognitoEvent(dataset *cognitosync.Dataset, records []
 		Data: cognitoEvent,
 	}
 
-	_, err := clients.CloudEvents.Send(context.Background(), event)
+	_, _, err := clients.CloudEvents.Send(context.Background(), event)
 	if err != nil {
 		return err
 	}
